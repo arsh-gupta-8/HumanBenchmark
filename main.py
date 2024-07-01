09d385f2d790163efbf5ba2efabbf9e537722c68
arsh-gupta-8 <SlurpIt.MBMS@gmail.com>
2024-07-01 16:00:11 +0700

    Finished sequence memory game

--- main.py
+++ main.py
@@ -33,12 +33,19 @@ FPS = 60
 # Game settings
 scroll = 0
 page = -1
-active_timer = -1
+score_keeper = -1
 hover = None
 game_start = None
 game_status = -1
-reaction_time = -1
+game_memory = -1
 game_message = ''
+
+# Sequence Memory Game exclusive settings
+sequence_boxes = []
+for i in range(3):
+    for j in range(3):
+        rectangle = pygame.Rect(320 + (125 * j), 110 + (125 * i), 110, 110)
+        sequence_boxes.append(rectangle)
 
 def display_msg(string, str_type, colour, x, y):
     write_line = str_type.render(string, False, colour)
@@ -49,14 +56,6 @@ def display_msg(string, str_type, colour, x, y):
 while running:
 
     option = []
-
-    if active_timer >= 0:
-        active_timer += 1
-
-    for i in range(4):
-        for j in range(2):
-            rectangle = pygame.Rect(100 + (425 * j), 510 + (250 * i) + scroll, 375, 200)
-            option.append(rectangle)
 
     x, y = pygame.mouse.get_pos()
 
@@ -80,21 +79,38 @@ while running:
                 page = -1
                 game_status = -1
                 game_start = None
-                reaction_time = -1
-                active_timer = -1
+                game_memory = -1
+                score_keeper = -1
 
         if event.type == pygame.MOUSEBUTTONUP:
 
             if event.button == 1:
                 if game_start:
 
-                    if game_status == 0:
-                        game_message = "Clicked too early"
-                    elif game_status == 1:
-                        game_message = str(math.floor((active_timer / FPS) * 1000)) + ' ms'
-                    game_status = 2
+                    if page == 0:
+                        if game_status == 0:
+                            game_message = "Clicked too early"
+                        elif game_status == 1:
+                            game_message = str(math.floor((score_keeper / FPS) * 1000)) + ' ms'
+                        game_status = 2
 
-                if hover != None:
+                    elif page == 1:
+                        if game_status == 1:
+
+                            if score_keeper[3] != None:
+                                score_keeper[4] += 1
+
+                                if score_keeper[3] == game_memory[score_keeper[4]]:
+                                    if score_keeper[4] + 1 == len(game_memory):
+                                        score_keeper[0] = 0
+                                        game_status = 0
+                                        score_keeper[1] = 0
+
+                                else:
+                                    game_message = "Level " + str(len(game_memory))
+                                    game_status = 2
+
+                elif hover != None:
                     page = hover
                     hover = None
                     game_start = False
@@ -113,6 +129,11 @@ while running:
         pygame.draw.rect(screen, (230, 232, 244), pygame.Rect(0, scroll+410, 1003, 1200))
 
         pygame.draw.rect(screen, (254,146,69), pygame.Rect(997, 0, 6, (scroll / 1040) * -500))
+
+        for i in range(4):
+            for j in range(2):
+                rectangle = pygame.Rect(100 + (425 * j), 510 + (250 * i) + scroll, 375, 200)
+                option.append(rectangle)
 
         for rect in range(8):
             if option[rect].collidepoint((x, y)):
@@ -139,27 +160,93 @@ while running:
                 screen.fill((231, 13, 1))
                 display_msg("Wait...", title, (255, 255, 255), 500, 200)
 
-                if reaction_time < 0:
-                    reaction_time = random.randint(60, 420)
-                elif reaction_time == 0:
+                if game_memory < 0:
+                    game_memory = random.randint(60, 420)
+                elif game_memory == 0:
                     game_status = 1
                 else:
-                    reaction_time -= 1
+                    game_memory -= 1
 
             elif game_status == 1:
 
                 screen.fill((62, 180, 137))
                 display_msg("Click", title, (255, 255, 255), 500, 200)
-                if active_timer < 0:
-                    active_timer = 0
+                if score_keeper < 0:
+                    score_keeper = 0
+                else:
+                    score_keeper += 1
             else:
                 display_msg(game_message, title, (255, 255, 255), 500, 200)
                 display_msg("Press enter to continue", key_instr, (255, 255, 255), 500, 320)
-                active_timer = -1
-                reaction_time = -1
+                score_keeper = -1
+                game_memory = -1
         else:
             display_msg("Reaction Time", title, (255, 255, 255), 500, 100)
             display_msg("Click when the screen turns green!", text, (255, 255, 255), 500, 220)
+            display_msg("Press enter to start", key_instr, (255, 255, 255), 500, 320)
+            display_msg("Press escape to go back", key_instr, (255, 255, 255), 500, 355)
+
+    elif page == 1:
+
+        if game_start:
+        
+            if game_status == 0:
+
+                if score_keeper[0] == 0:
+                    new_square = random.randint(0, 8)
+                    if len(game_memory) > 0:
+                        while new_square == game_memory[-1]:
+                            new_square = random.randint(0, 8)
+                    game_memory.append(new_square)
+                    score_keeper[0] = 1
+
+                display_msg("Level " + str(len(game_memory)), text, (255, 255, 255), 500, 50)
+
+                if score_keeper[2] == -1:
+                    score_keeper[2] = 1
+                elif score_keeper[2] % 40 == 0:
+                    score_keeper[1] += 1
+                    score_keeper[2] += 1
+                else:
+                    score_keeper[2] += 1
+
+                if score_keeper[1] == len(game_memory):
+                    score_keeper[4] = -1
+                    score_keeper[2] = -1
+                    game_status = 1
+                    continue
+
+                for rect in range(9):
+                    if rect == game_memory[score_keeper[1]]:
+                        pygame.draw.rect(screen, (230, 232, 244), sequence_boxes[rect])
+                    else:
+                        pygame.draw.rect(screen, (34,108,180), sequence_boxes[rect])
+
+            elif game_status == 1:
+
+                display_msg("Level " + str(len(game_memory)), text, (255, 255, 255), 500, 50)
+
+                for square in range(9):
+                    if sequence_boxes[square].collidepoint((x, y)):
+                        score_keeper[3] = square
+                        pygame.draw.rect(screen, (34,108,180), sequence_boxes[square])
+                    else:
+                        pygame.draw.rect(screen, (37,115,193), sequence_boxes[square])
+
+                if screen.get_at((x, y)) == (43, 135, 209):
+                    score_keeper[3] = None
+
+            else:
+                display_msg(game_message, title, (255, 255, 255), 500, 200)
+                display_msg("Press enter to continue", key_instr, (255, 255, 255), 500, 320)
+                score_keeper = -1
+                game_memory = -1
+
+        else:
+            game_memory = []
+            score_keeper = [0, 0, -1, 0, -1]
+            display_msg("Sequence Memory", title, (255, 255, 255), 500, 100)
+            display_msg("Memorise the increasing pattern!", text, (255, 255, 255), 500, 220)
             display_msg("Press enter to start", key_instr, (255, 255, 255), 500, 320)
             display_msg("Press escape to go back", key_instr, (255, 255, 255), 500, 355)
 
